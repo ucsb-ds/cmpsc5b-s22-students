f2f5f36d0fc3fe7bb4f62f52a91ea1a723e6f139
Nagarjun Avaraddy <nagarjun@umail.ucsb.edu>
2022-04-20 02:48:28 -0700

    midterm-hw02-midtermworksheet

---
+++ projects/midterm project/tests/q1_1.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q1_1',
+    'points': None,
+    'suites': [   {   'cases': [{'code': '>>> accidents.num_rows == 2\nTrue', 'hidden': False, 'locked': False}, {'code': '>>> len(accidents.labels) == 2\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ projects/midterm project/tests/q2_6.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q2_6',
+    'points': None,
+    'suites': [   {   'cases': [   {'code': '>>> len(bootstrap_cds) == 1000\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> np.random.seed(100)\n>>> np.isclose(round(one_bootstrap_cd(), 3), 60256.49)\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
