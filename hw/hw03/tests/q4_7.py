0ecbbe470163bec6d6ec6613ba6d3389d87e49be
ynchn <yanchenlu@ucsb.edu>
2022-05-04 17:09:43 +0000

    add hw03 files

---
+++ hw/hw03/tests/q4_7.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q4_7',
+    'points': None,
+    'suites': [{'cases': [{'code': '>>> type(sample_size_calculation) == int\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
