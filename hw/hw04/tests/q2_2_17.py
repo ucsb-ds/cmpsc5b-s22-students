3736ed6fdb79925146d6dc42ced35a953601486c
Nagarjun Avaraddy <nagarjun@umail.ucsb.edu>
2022-05-11 10:30:45 -0700

    hw04 and lab03

---
+++ hw/hw04/tests/q2_2_17.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q2_2_17',
+    'points': None,
+    'suites': [{'cases': [{'code': '>>> type(same_parameters) == bool\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
---
+++ lab/lab03/tests/q2_7.py
@@ -0,0 +1,1 @@
+test = {'name': 'q2_7', 'points': None, 'suites': [{'cases': [{'code': '>>> q2_7 == 1\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
